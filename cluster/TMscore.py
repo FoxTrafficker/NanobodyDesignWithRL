2100fc42c49fc9b1c4cfdbb2f3baff056417522a
FoxTrafficker <150244675+FoxTrafficker@users.noreply.github.com>
2024-04-14 10:25:58 +0900

    Add files via upload

---
+++ cluster/TMscore.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+def TMscore(chainA,chainB):
+
+    return tm_score
+
+if __name__ == '__main__'():
+    process = subprocess.Popen(['echo', 'Hello, World!'], stdout=subprocess.PIPE, text=True)
+    output, _ = process.communicate()
+
